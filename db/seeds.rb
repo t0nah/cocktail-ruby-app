32acbd95f5f991af93edbe02955c1986a30225be
Treasure <treasureonahj@gmail.com>
2024-10-28 08:58:14 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'net/http'
+require 'json'
+
+def fetch_categories
+  url = URI("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+  response = Net::HTTP.get(url)
+  data = JSON.parse(response)
+
+  data["drinks"].each do |category|
+    Category.create!(name: category["strCategory"])
+  end
+end
+
+def fetch_glasses
+  url = URI("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+  response = Net::HTTP.get(url)
+  data = JSON.parse(response)
+
+  data["drinks"].each do |glass|
+    Glass.create!(name: glass["strGlass"])
+  end
+end
+
+def fetch_ingredients
+  url = URI("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+  response = Net::HTTP.get(url)
+  data = JSON.parse(response)
+
+  data["drinks"].each do |ingredient|
+    Ingredient.create!(name: ingredient["strIngredient1"])
+  end
+end
+
+def fetch_alcoholic_filters
+  url = URI("https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list")
+  response = Net::HTTP.get(url)
+  data = JSON.parse(response)
+
+  data["drinks"].each do |filter|
+    AlcoholicFilter.create!(name: filter["strAlcoholic"])
+  end
+end
+
+fetch_categories
+fetch_glasses
+fetch_ingredients
+fetch_alcoholic_filters
