9e4652f0184129ecd0af7cb7709ca84331b30be5
Treasure <treasureonahj@gmail.com>
2024-10-30 23:54:09 -0500

    Resolved issues displaying data rows

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem 'bootstrap', '~> 5.3'
 gem 'sassc-rails'
+gem 'kaminari'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.1"
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,15 +3,28 @@ require 'json'
 
 class CocktailsController < ApplicationController
   def index
-    url = URI("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a") # This fetches cocktails starting with the letter "A"
+    all_cocktails = []
 
-    # Make the API request
-    response = Net::HTTP.get(url)
-    @cocktails = JSON.parse(response)["drinks"] # Store the data in an instance variable to pass to the view
+    ('a'..'z').each do |letter|
+      break if all_cocktails.size >= 100  #  100 cocktails
+
+      url = URI("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}")
+
+      # Make the API request
+      response = Net::HTTP.get(url)
+      data = JSON.parse(response)
+
+      if data["drinks"]
+        all_cocktails.concat(data["drinks"])
+        all_cocktails = all_cocktails.take(100)
+      end
+    end
+
+    @cocktails = all_cocktails
   end
 
+
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def search
